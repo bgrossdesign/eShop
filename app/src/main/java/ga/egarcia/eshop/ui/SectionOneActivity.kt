7df3835f1c42ba30c1a7b1391dcb706686e51f76
Anne Gross <annegross@anne-macbook.local>
2021-12-12 23:14:21 -0700

    Added base project structure

---
+++ app/src/main/java/ga/egarcia/eshop/ui/SectionOneActivity.kt
@@ -0,0 +1,14 @@
+package ga.egarcia.eshop.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ga.egarcia.eshop.R
+
+
+class SectionOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_section_one)
+    }
+}
